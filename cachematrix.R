0d84384bcf39ad620d2c4f28e7a031ccc4cf4f35
cjosmoreno <cjosmoreno@users.noreply.github.com>
2015-07-25 07:42:41 -0700

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+##this function is goinng to simplify the way we obtain inverse matrix
+##It will use tha cache method in order to save previuos results
+
+## This function will create the cache matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+              m=NULL
+              set = function(y){
+                x<<- y
+                m<<-NULL
+                }
+            
+                get= function() x
+                setmean= function(solve) m <<- mean
+                getmean= function() m
+                list(set=set,get=get,setmean=setmean,getmean=getmean)
+       
+}
+
+
+## This function is going to calculate the inverse and check if previuos calculate
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m= x$getmean()
+       if (!is.null(m)){
+                  message("getting cached data")
+                  return(m)}
+       data=x$get()
+        m=solve(data,...) 
+        x$setmean(m) 
+        m
+}
